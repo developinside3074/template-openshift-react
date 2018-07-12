c3afd24ad468bf3f3034ca64e0f7343f58bcc18f
Osmar Gonzalez <developinside3074@gmail.com>
2018-07-12 16:34:47 -0500

    Refactorizacion a las 16:34 pm

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hola Attlaris</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Esto es una prueba de React en Openshift
         </p>
       </div>
     );
